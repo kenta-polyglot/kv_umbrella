66d3d897a9f4bf04a84d85e6a2af0e3825506b76
Kenta Katsumata <polyglot.software@gmail.com>
2016-04-10 14:28:26 +0900

    Docs, tests and with

--- apps/kv_server/test/kv_server_test.exs
+++ apps/kv_server/test/kv_server_test.exs
@@ -1,8 +1,52 @@
 defmodule KVServerTest do
+  # This time, since our test relies on global data, we have not given async: true to use ExUnit.Case
   use ExUnit.Case
-  doctest KVServer
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  # To avoid printing log messages during tests. like below:
+  # 12:49:10.785 [info]  Application kv exited: :stopped
+  @moduletag :capture_log
+  
+  setup do
+    Application.stop(:kv)
+    :ok = Application.start(:kv)
+  end
+
+  setup do
+    opts = [:binary, packet: :line, active: false]
+    {:ok, socket} = :gen_tcp.connect('localhost', 4040, opts)
+    {:ok, socket: socket}
+  end
+
+  # as with ETS tables and linked processes, there is no need to close the socket. 
+  # Once the test process exits, the socket is automatically closed.
+  test "server interaction", %{socket: socket} do
+    assert send_and_recv(socket, "UNKNOWN shopping\r\n") ==
+           "UNKNOWN COMMAND\r\n"
+
+    assert send_and_recv(socket, "GET shopping eggs\r\n") ==
+           "NOT FOUND\r\n"
+
+    assert send_and_recv(socket, "CREATE shopping\r\n") ==
+           "OK\r\n"
+
+    assert send_and_recv(socket, "PUT shopping eggs 3\r\n") ==
+           "OK\r\n"
+
+    # GET returns two lines
+    assert send_and_recv(socket, "GET shopping eggs\r\n") == "3\r\n"
+    assert send_and_recv(socket, "") == "OK\r\n"
+
+    assert send_and_recv(socket, "DELETE shopping eggs\r\n") ==
+           "OK\r\n"
+
+    # GET returns two lines
+    assert send_and_recv(socket, "GET shopping eggs\r\n") == "\r\n"
+    assert send_and_recv(socket, "") == "OK\r\n"
+  end
+
+  defp send_and_recv(socket, command) do
+    :ok = :gen_tcp.send(socket, command)
+    {:ok, data} = :gen_tcp.recv(socket, 0, 1000)
+    data
   end
 end
